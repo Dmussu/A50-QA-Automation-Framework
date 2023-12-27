2719bd8a5cbd8d6fd18f17ba1deb0574117b0ef7
Dmussu <mussulmanovam9@gmail.com>
2023-12-27 09:59:35 -0600

    Rename Playlist Test

--- src/test/java/Homework21Test.java
+++ src/test/java/Homework21Test.java
@@ -1,12 +1,13 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
 
 public class Homework21Test extends BaseTest {
 
@@ -17,24 +18,32 @@ public class Homework21Test extends BaseTest {
         WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(20));
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//li[contains(@class, 'playlist')]//a[contains(text(),'HW17')]")));
 
-        WebElement rightClickOnSelectedPlaylist = getDriver().findElement(By.xpath("//li[contains(@class, 'playlist')]//a[contains(text(),'HW17')]"));
-        Actions actions = new Actions(getDriver());
-        actions.contextClick(rightClickOnSelectedPlaylist).perform();
+        List<WebElement> playlists = getDriver().findElements(By.xpath("//li[contains(@class, 'playlist')]"));
+
+        for (WebElement playlist : playlists) {
+            if (playlist.getText().equals("HW17")) {
+                actions.contextClick(playlist).perform();
+            }
+        }
 
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[contains(text(), 'Edit')]")));
 
         WebElement editButtonClick = getDriver().findElement(By.xpath("//li[contains(text(), 'Edit')]"));
         editButtonClick.click();
 
-        WebElement playListNameInputField = getDriver().findElement(By.xpath("//input[@name='name']"));
-        actions.moveToElement(playListNameInputField);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[name='name']")));
+        WebElement playListNameInputField = getDriver().findElement(By.cssSelector("[name='name']"));
 
         actions.moveToElement(playListNameInputField)
                 .doubleClick().perform();
         actions.moveToElement(playListNameInputField)
+                .sendKeys(Keys.COMMAND, "a")
                 .sendKeys(Keys.DELETE)
                 .sendKeys("HW21")
                 .sendKeys(Keys.ENTER)
                 .build().perform();
+
+        WebElement renamedPlaylist = getDriver().findElement(By.xpath("//li[contains(@class, 'playlist')]//a[contains(text(),'HW21')]"));
+        Assert.assertTrue(renamedPlaylist.isDisplayed(), "Playlist has not been renamed to 'HW21'");
     }
 }
